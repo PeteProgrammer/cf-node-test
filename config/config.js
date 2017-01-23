4a39eb85d993bb561e6546e79d7ee5c19d34a022
Peter Strøiman <peter@stroiman.com>
2017-01-23 14:14:52 +0000

    Simplified config

--- config/config.js
+++ config/config.js
@@ -1,39 +1,12 @@
 var path = require('path'),
     cfenv = require('cfenv'),
-    rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+    rootPath = path.normalize(__dirname + '/..');
 
-var config = {
-  development: {
+module.exports = {
     root: rootPath,
     app: {
       name: 'node-app'
     },
     port: process.env.PORT || 3000,
-    db: 'mysql://localhost/node-app-development'
-  },
-
-  test: {
-    root: rootPath,
-    app: {
-      name: 'node-app'
-    },
-    port: process.env.PORT || 3000,
-    db: 'mysql://localhost/node-app-test'
-  },
-
-  production: {
-    root: rootPath,
-    app: {
-      name: 'node-app'
-    },
-    port: process.env.PORT || 3000,
-    db: 'mysql://localhost/node-app-production'
-  }
+    db: cfenv.getAppEnv().getService('db-small').credentials.uri;
 };
-
-console.log("Services content: ", process.env.VCAP_SERVICES)
-svc = JSON.parse(process.env.VCAP_SERVICES)
-config[env].db = cfenv.getAppEnv().getService('db-small').credentials.uri;
-
-module.exports = config[env];
